fd48f718d3e5760f47adcc45f6179f429de74518
moonbek007 <65666173+moonbek007@users.noreply.github.com>
2021-07-22 15:42:28 +0500

    feat: add components and router

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,47 @@
-import "./App.css";
+import "./css/styles.css";
 import React from "react";
 import firebase from "firebase";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Header from "./components/Header/Header";
+import MainPage from "./components/MainPage/MainPage";
+import Filters from "./components/Filters/Filters";
+import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   React.useEffect(() => {
     const db = firebase.database();
+    console.log(db);
   }, []);
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <header className="header">
+          <Route>
+            <Header/>
+          </Route>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/">
+              <MainPage/>
+            </Route>
+            <Route path="/search">
+              <Filters/>
+            </Route>
+            <Route path="/login">
+              <Login/>
+            </Route>
+          </Switch>
+        </main>
+        <footer className="footer">
+          <Route>
+            <Footer/>
+          </Route>
+        </footer>
+      </Router>
+    </>
+  )
 }
 
 export default App;
