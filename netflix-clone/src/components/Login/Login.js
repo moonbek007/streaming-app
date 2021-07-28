3de0ea583513f0b66f690cd0b47ca1492af0d2e5
moonbek007 <65666173+moonbek007@users.noreply.github.com>
2021-07-28 09:40:30 +0500

    feat: do login page markup

--- netflix-clone/src/components/Login/Login.js
+++ netflix-clone/src/components/Login/Login.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { connect, useDispatch } from "react-redux";
+import LoginForm from "./LoginForm";
+import RegistrationForm from "./RegistrationForm";
+import "../../css/login.css";
 
-function Login() {
-    return (
-        <div className="login">
-            <h1>Login</h1>
-        </div>
-    )
+function Login({ credentials }) {
+  const [formToShow, setFormToShow] = React.useState("login");
+  return (
+    <>
+      <div className="login">
+        <h1>{formToShow === "login" ? "Sign in" : "Sign up"}</h1>
+        {formToShow === "login" ? <LoginForm /> : <RegistrationForm />}
+        <button className="login__btn">
+          {formToShow === "login" ? "Sign in" : "Sign up"}
+        </button>
+        or
+        <button id="signup-btn" onClick={() => setFormToShow("signup")}>
+          Sign Up
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Login
+const mapStateToProps = (state) => ({
+  credentials: state.credentials,
+});
+
+export default connect(mapStateToProps)(Login);
